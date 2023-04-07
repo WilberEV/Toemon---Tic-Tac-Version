255550b2db6039da60e08e86d00c9b1e4ed3a3cf
WilberEV <wilberesparragoza@gmail.com>
2023-04-07 17:28:57 +0200

    Code for AI games v3

--- main.ts
+++ main.ts
@@ -8,14 +8,19 @@ let board = [
   [0, 0, 0],
 ];
 let playerTurn = 1;
-let NPC = false;
+let NPC = true;
 let p1Tokens = 3;
 let p2Tokens = 3;
+
+
 
 const toggle = (from, to) => {
   document.querySelector(from).classList.add("none");
   document.querySelector(to).classList.remove("none");
 };
+
+
+
 
 const introText = () =>{
   let textArr = [
@@ -50,20 +55,29 @@ const introText = () =>{
   introPage++
 }
 
+
+
+
+
 const selecMC = (gender) =>{
   mainChara = gender;
   introPage++
   if (gender == 1){
     toggle("#carmine", '#crimson')
     document.querySelector("#crimson .sprites").setAttribute("onclick", "")
-
+    introText()
+    return
   } else {
     toggle('#crimson', '#carmine')
     document.querySelector("#carmine .sprites").setAttribute("onclick", "")
     document.querySelector("#p1Side .sprites img").setAttribute("src", "img/sprites/carmine.png");
+    introText()
+    return
   }
-  introText()
 }
+
+
+
 
 const setName = () =>{
    if(p1Name == ""){
@@ -76,49 +90,65 @@ const setName = () =>{
    }
 }
 
+
+
+
+
 const play = (fil, col) => {
   let resF = fil + 1;
   let resC = col + 1;
 
   if(checkWinner(board) == false){
     if (playerTurn == 1) {
-      if (p1Tokens > 0) {
+      if (p1Tokens > 0 && board[fil][col] == 0) {
         board[fil][col] = 1;
-        playerTurn = playerTurn * -1;
         document.querySelector(`#board :nth-child(${resF}) :nth-child(${resC}) img`).setAttribute("src", "img/background/water.png");
         document.querySelector(`#p2Side .tokenCount :nth-child(${p1Tokens})`).setAttribute("src", "img/background/emptyToken.png");
         p1Tokens = p1Tokens - 1;
+        playerTurn = playerTurn * -1;
+        
       } else if (p1Tokens == 0) {
         if (board[fil][col] == 1) {
           board[fil][col] = 0;
           document.querySelector(`#board :nth-child(${resF}) :nth-child(${resC}) img`).setAttribute("src", "");
           p1Tokens = p1Tokens + 1;
           document.querySelector(`#p2Side .tokenCount :nth-child(${p1Tokens})`).setAttribute("src", "img/background/fullToken.png");
+          
         }
       }
     } else if (playerTurn == -1) {
-      if (p2Tokens > 0) {
+      if (p2Tokens > 0 && board[fil][col] == 0) {
         board[fil][col] = 2;
         playerTurn = playerTurn * -1;
         if (NPC == true) {
           document.querySelector(`#board :nth-child(${resF}) :nth-child(${resC}) img`).setAttribute("src", "img/background/grass.webp");
-        } else
+        } else{
           document.querySelector(`#board :nth-child(${resF}) :nth-child(${resC}) img`).setAttribute("src", "img/background/fire.png");
-          document.querySelector(`#p1Side .tokenCount :nth-child(${p2Tokens})`).setAttribute("src", "img/background/emptyToken.png");
+        }
+        document.querySelector(`#p1Side .tokenCount :nth-child(${p2Tokens})`).setAttribute("src", "img/background/emptyToken.png");
         p2Tokens = p2Tokens - 1;
+        
       } else if (p2Tokens == 0) {
         if (board[fil][col] == 2) {
           board[fil][col] = 0;
           document.querySelector(`#board :nth-child(${resF}) :nth-child(${resC}) img`).setAttribute("src", "");
           p2Tokens = p2Tokens + 1;
           document.querySelector(`#p1Side .tokenCount :nth-child(${p2Tokens})`).setAttribute("src", "img/background/fullToken.png");
+
         }
       }
     }
-  checkWinner(board);
-  turn(playerTurn);
+    console.log(board)
+    checkWinner(board);
+    console.log(playerTurn)
+    turn(playerTurn);
+    if(NPC == true){
+      playNPC(board);
+    }
   }
 };
+
+
 
 
 const checkWinner = (board):boolean => {
@@ -138,49 +168,77 @@ const checkWinner = (board):boolean => {
   } else return false;
 };
 
+
+
+
+
+
 const playNPC = (board) => {
 
-  if (p2Tokens == 3){
-    let X = Math.round((Math.random() * 10)/3)
-    let Y = Math.round((Math.random() * 10)/3)
-    if (board[X][Y] == 0) {
-      board[X][Y] = 2;
-    } else {
-      playNPC(board);
-    }
-  } else if(p2Tokens < 3) {
+  if (p2Tokens == 3 && playerTurn == -1){
+    let X = Math.round(Math.random() * 2)
+    console.log(X)
+    let Y = Math.round(Math.random() * 2)
+    console.log(Y)
+    play(X,Y);
+    return
+  } else if(p2Tokens < 3 && playerTurn == -1) {
     for (let i = 0; i < board.length; i++) {
       if (board[i][0] === board[i][1] || board[i][1] === board[i][2] || board[i][0] === board[i][2]){
         if (board[i][0] == 0){
           board[i][0] = 2;
+          play(i,0);
+          return
         } else if (board[i][1] == 0){
           board[i][1] = 2;
+          play(i,1);
+          return
         } else if (board[i][2] == 0){
-          board[i][0] = 2;
+          board[i][2] = 2;
+          play(i,2);
+          return
         }
       } else if (board[0][i] === board[1][i] || board[1][i] === board[2][i] || board[0][i] === board[2][i]){
         if (board[0][i] == 0){
           board[0][i] = 2;
+          play(0,i);
+          return
         } else if (board[1][i] == 0){
           board[1][i] = 2;
+          play(1,i);
+          return
         } else if (board[2][i] == 0){
           board[2][i] = 2;
+          play(2,i);
+          return
         }
       } else if (board[0][0] === board[1][1] || board[1][1] === board[2][2] || board[0][0] === board[2][2]){
         if (board[0][0] == 0){
           board[0][0] = 2;
+          play(0,0);
+          return
         } else if (board[1][1] == 0){
           board[1][1] = 2;
+          play(1,1);
+          return
         } else if (board[2][2] == 0){
           board[2][2] = 2;
+          play(2,2);
+          return
         }
       } else if (board[0][2] === board[1][1] || board[1][1] === board[2][0] || board[0][2] === board[2][0]){
         if (board[0][2] == 0){
           board[0][2] = 2;
+          play(0,2);
+          return
         } else if (board[1][1] == 0){
           board[1][1] = 2;
+          play(1,1);
+          return
         } else if (board[2][0] == 0){
           board[2][0] = 2;
+          play(2,0);
+          return
         }
       }
     }
@@ -188,35 +246,39 @@ const playNPC = (board) => {
   turn(playerTurn);
 };
 
+
+
+
+
 const turn = (player) => {
   if(checkWinner(board) == false){   
     if (player == 1) {
       document.querySelector("#gameBoard .overlayText").innerHTML = `It's  ${p1Name}'s turn!`;
+      return
     }
     if (player == -1) {
       document.querySelector("#gameBoard .overlayText").innerHTML = `It's  ${p2Name}'s turn!`;
-      if(NPC == true){
-        playNPC(board);
-      }
+      return
     }
     if (player == 1 && p1Tokens == 0) {
       document.querySelector("#gameBoard .overlayText").innerHTML =
       `It's ${p1Name}'s turn! But... They don't have any token left! Please select a Token to remove!`;
+      return
     }
     if (player == -1 && p2Tokens == 0) {
       document.querySelector("#gameBoard .overlayText").innerHTML =
       `It's ${p2Name}'s turn! But... They don't have any token left! Please select a Token to remove!`;
-      if(NPC == true){
-        playNPC(board);
-      }
+      return
     }
   } else {
     player = player * -1;
     if (player == 1){
       document.querySelector("#gameBoard .overlayText").innerHTML = `The winner is ${p1Name}!`;
+      return
     }
     if (player == -1) {
       document.querySelector("#gameBoard .overlayText").innerHTML = `The winner is ${p2Name}!`;
+      return
     }
   } 
 };
